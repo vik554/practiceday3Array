3854f411caf4f307d77645035e740e6dfff8f172
vik554 <kumbharvikram27@gmail.com>
2023-05-14 18:01:40 +0530

    ADDED:code to smallest element

--- src/practiceArray/JavaArray.java
+++ src/practiceArray/JavaArray.java
@@ -6,6 +6,7 @@ public class JavaArray {
         s1.printarray();
         s1.frequencyofarray();
         s1.largestelement();
+        s1.smallestelement();
 
     }
 
@@ -46,6 +47,7 @@ public class JavaArray {
         }
         return frequencyArr;
     }
+
     public void largestelement() {
         int max = 0;
         int a[] = {44, 3, 6, 55, 43, 22};
@@ -58,5 +60,28 @@ public class JavaArray {
             }
         }
         System.out.println("Maximum value in the array is:" + max);
+    }
+
+    public void smallestelement() {
+        int min = 0;
+        int a[] = {44, 3, 6, 55, 43, 22};
+        for (int i = 0; i < a.length; i++)
+            min = a[0];
+
+        for (int i = 0; i < a.length; i++) {
+            if (min > a[i]) {
+                min = a[i];
+            }
+        }
+        System.out.println("Minimum value in the array is:" + min);
+    }
+
+    public void even_positon() {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+
+        System.out.println("Elements at even positions:");
+        for (int i = 1; i < arr.length; i += 2) {
+            System.out.println(arr[i]);
+        }
     }
 }
